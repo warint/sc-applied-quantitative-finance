1c1b82e9617b22ccd48bdb98cfc093e0fdb87d01
Thierry Warin <thierry.warin@nuance-r.com>
2023-01-29 17:15:08 -0500

    u

--- prophet/SP500_v1.R
+++ prophet/SP500_v1.R
@@ -1,10 +1,35 @@
-# resources
-# https://www.r-bloggers.com/2022/03/new-r-package-yfr/
 
+library(prophet)
+library(dplyr)
 library(yfR)
 
-df.stocks <- yf_collection_get("SP500", 
-                           first_date = Sys.Date() - 360,
-                           last_date = Sys.Date())
+# set options for algorithm
+my_ticker <- 'TSLA'
+first_date <- Sys.Date() - 360
+last_date <- Sys.Date()
 
-head(df.stocks)
+# fetch data
+stats <- yf_get(tickers = my_ticker, 
+                first_date = first_date,
+                last_date = last_date)
+
+head(stats)
+
+#daily data points starting from 2014–01–0
+
+stats <- stats |> mutate(ds = ref_date) |> mutate(y = price_adjusted)
+#View(summary(stats))
+plot(y ~ ds, stats, type = "l")
+
+
+m <- prophet(stats, daily.seasonality=TRUE, yearly.seasonality=TRUE)
+
+future <- make_future_dataframe(m, periods = 90)
+forecast <- predict(m, future)
+
+plot(m, forecast)
+
+tail(forecast[c('ds', 'yhat', 'yhat_lower', 'yhat_upper')])
+tail(forecast)
+
+prophet_plot_components(m, forecast)
